f5cf6b74f6275a89e9be00e0080ec9a03640446a
BreMea <63671187+BreMea@users.noreply.github.com>
2022-02-26 21:03:43 -0500

    feat: front-end mockup

--- src/pages/api/packet.ts
+++ src/pages/api/packet.ts
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { db } from '@/lib/database/Database/Database';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,26 @@ export default function HomePage() {
   return (
     <Layout>
       <Seo />
-      <main>
-        <section>
-          <div className='layout flex min-h-screen w-96 flex-col items-center justify-center text-center'>
+      <main className='w-full'>
+        <section className='w-full'>
+          <div className='layout flex min-h-screen w-full flex-col items-center justify-center text-center'>
             <h1 className='mt-4 text-primary'>Crowd</h1>
-            <p>coming soon!</p>
-            <footer className='absolute bottom-2 text-gray-700'>
+            <div className='mt-12 flex w-full items-center justify-between rounded-full p-8 shadow-xl'>
+              <div className='flex flex-col items-start'>
+                <p className='font-bold'>Dining Hall</p>
+                <p className='text-warning'>Very Busy</p>
+              </div>
+              <div
+                className='radial-progress text-warning'
+                style={{ '--value': '72' } as React.CSSProperties}
+              >
+                72%
+              </div>
+            </div>
+            <footer className='absolute bottom-2 text-sm text-gray-700'>
               © {new Date().getFullYear()} Brett Meadows
+              <br />
+              Made for Buildergroop Hackathon #1
             </footer>
           </div>
         </section>
